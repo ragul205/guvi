1d0163eed7fe1259ee9e10fd51ce5c6df6559a06
ragul205 <ragulrakesh.06@gmail.com>
2016-06-07 14:23:10 +0530

    Create holiday.py

---
+++ holiday.py
@@ -0,0 +1,15 @@
+day=input()
+if(day=='Monday'):
+    print ("true")
+elif(day=='Tuesday'):
+     print("true")
+elif(day=='Wednesday'):
+     print("true")
+elif(day=='Thursday'):
+     print("true")
+elif(day=='Friday'):
+     print("true")
+elif(day=='Saturday'):
+     print("False")
+elif(day=='Sunday'):
+     print("False")
